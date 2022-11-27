6b1e9567fb77c9f106e671e0c8ec07fb227a689b
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-27 11:54:17 +0600

    api data load

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('exDesktopAccessories').collection('categories');
+
+        // categories
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const options = await categoriesCollection.find(query).toArray();
+            res.send(options);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error());
+
+
 app.get('/', async (req, res) => {
     res.send('exDesktop accessories server is running')
 })
